0c12911c17d4127794e2b63897ee4e6ef890d50e
Vyacheslav <lance110741@gmail.com>
2025-01-30 22:16:17 +0300

    Добавлены функции в кейсы

--- ChatConsole/main.cpp
+++ ChatConsole/main.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
+#include <vector>
 #include "Chat.h"
 using namespace std;
 
 int main()
 {
-    cout << "Добро пожаловать в Чат" << endl;
-    cout << "Войдите или авторизируйтесь" << endl;
-    cout << "1. Регистрация" << endl;
-    cout << "2. Авторизация" << endl;
-    cout << "3. Отправить сообщение пользователю" << endl;
-    cout << "4. Отправить сообщение в чат" << endl;
-    cout << "Для выхода введите '0' " << endl;
+    // cout << "Добро пожаловать в Чат" << endl;
+    // cout << "Войдите или авторизируйтесь" << endl;
+    // cout << "1. Регистрация" << endl;
+    // cout << "2. Авторизация" << endl;
+    // cout << "3. Отправить сообщение пользователю" << endl;
+    // cout << "4. Отправить сообщение в чат" << endl;
+    // cout << "Для выхода введите '0' " << endl;
 
     Chat chat;
+
     while (true)
     {
+        cout << "Добро пожаловать в Чат" << endl;
+        cout << "Войдите или авторизируйтесь" << endl;
+        cout << "1. Регистрация" << endl;
+        cout << "2. Авторизация" << endl;
+        cout << "3. Отправить сообщение пользователю" << endl;
+        cout << "4. Отправить сообщение в чат" << endl;
+        cout << "Для выхода введите '0' " << endl;
+
         int choise;
         cin >> choise;
 
         std::string login;
         std::string password;
         std::string name;
+        std::string text;
         if (choise == 0)
         {
             cout << "Выход из программы" << endl;
@@ -43,7 +54,8 @@ int main()
             cin >> name;
             // функция регистрации пользователя
             chat.registerUser(login, password, name);
-            cout << "Вы успешно зарегистрировались" << endl;
+            // cout << "Вы успешно зарегистрировались" << endl;
+            cout << endl;
             break;
         case 2:
             cout << "Введите логин: " << endl;
@@ -51,18 +63,23 @@ int main()
             cout << "Введите пароль: " << endl;
             cin >> password;
             // функция авторизации пользователя
-            
-            cout << "Вы успешно вошли" << endl;
+            chat.loginUser(login, password);
+            cout << endl;
             break;
         case 3:
-            cout << "Отправить сообщение пользователю" << endl;
+            cout << "Введите получателя:" << endl;
+            cin >> login;
+            cout << "Введите текст сообщения: " << endl;
+            cin >> text;
             // функция ввода сообщения и отправки пользоавтелю
-            
+            chat.sendToUser(login, text);
+            cout << endl;
             break;
         case 4:
             cout << "Отправить сообщение в чат" << endl;
             // функция ввода сообщения и отправки в чат
             
+            cout << endl;
             break;
         default:
             break;
