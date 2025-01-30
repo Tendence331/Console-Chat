bbdbb4a43f3554c891487e907630fedd143b24ac
Vyacheslav <lance110741@gmail.com>
2025-01-30 22:14:26 +0300

    Добавлены сеттеры

--- ChatConsole/Message.h
+++ ChatConsole/Message.h
@@ -7,9 +7,17 @@ class Message
 public:
     Message(const std::string& recipient, const std::string& text, const std::string& sender);
     ~Message() = default;
+
+    // getter
     std::string getRecipient() const;
     std::string getText() const;
     std::string getSender() const;
+
+    //setter
+    void setRecipient(const std::string recipient);
+    void setText(const std::string text);
+    void setSender(const std::string sender);
+
     void sendMessages();        // просмотр отправленного сообщения
 private:
     std::string _recipient;     // получатель
