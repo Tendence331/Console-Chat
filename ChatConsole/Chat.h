4e3ad0314abc524e355e62c35a85d794b71e4620
Vyacheslav <lance110741@gmail.com>
2025-01-30 22:12:29 +0300

    Обновлены методы

--- ChatConsole/Chat.h
+++ ChatConsole/Chat.h
@@ -4,23 +4,21 @@
 #include "User.h"
 #include "Message.h"
 
+class User;
 
 class Chat
 {
 public:
     Chat() = default;
-    Chat();
 
     void registerUser(std::string& login, std::string& password, std::string& name);   // регистрация пользователя
-    bool loginUser(std::string& login, std::string& password);   // авторизация пользователя
-
-    bool checkUser();           // проверка регистрации пользователя
+    bool loginUser(std::string& login, std::string& password);   // авторизация пользователя должен возвращать true если удачно, если нет то false
      
-    void sendToUser();          // отправка сообщения пользователю - сначала надо if(!currentUsers) Вы не авторизированы
+    void sendToUser(std::string& recipient, std::string& text);          // отправка сообщения пользователю - сначала надо if(!currentUsers) Вы не авторизированы
     void sendToChat();          // отправка сообщения в чат - сначала надо if(!currentUsers) Вы не авторизированы
     ~Chat() = default;
 private:
-    Chat* _currentUsers = nullptr;                                
-    std::shared_ptr<User> user;
-    std::shared_ptr<Message> message;
+    User *_currentUsers = {nullptr};    // текущий пользователь                         
+    std::shared_ptr<User> user;         // зарегистрированные пользователи
+    std::shared_ptr<Message> message;   // сообщения
 };
