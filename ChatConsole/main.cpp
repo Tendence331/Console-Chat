428bea282d582f0224c821cb567b5c46973a10d0
Vyacheslav <lance110741@gmail.com>
2025-02-01 01:23:49 +0300

    Добавление функционала

--- ChatConsole/main.cpp
+++ ChatConsole/main.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
-#include <vector>
 #include "Chat.h"
 using namespace std;
 
 int main()
 {
-    // cout << "Добро пожаловать в Чат" << endl;
-    // cout << "Войдите или авторизируйтесь" << endl;
-    // cout << "1. Регистрация" << endl;
-    // cout << "2. Авторизация" << endl;
-    // cout << "3. Отправить сообщение пользователю" << endl;
-    // cout << "4. Отправить сообщение в чат" << endl;
-    // cout << "Для выхода введите '0' " << endl;
-
+    
     Chat chat;
 
     while (true)
     {
-        cout << "Добро пожаловать в Чат" << endl;
+        cout << "-----------------------------" << endl;
         cout << "Войдите или авторизируйтесь" << endl;
         cout << "1. Регистрация" << endl;
         cout << "2. Авторизация" << endl;
         cout << "3. Отправить сообщение пользователю" << endl;
-        cout << "4. Отправить сообщение в чат" << endl;
+        cout << "4. Просмотр сообщений" << endl;
+        cout << "5. Просмотр всех пользователей" << endl;
         cout << "Для выхода введите '0' " << endl;
-
+        cout << "Выберите действие: ";
         int choise;
         cin >> choise;
+        cout << endl;
+
 
         std::string login;
         std::string password;
@@ -37,48 +32,61 @@ int main()
             cout << "Выход из программы" << endl;
             break;
         }
-        else if (choise > 4)
+        else if (choise > 5)
         {
             cout << "Неверно! Выберите Вход или Регистрацию!" << endl;
         }
         
-        
         switch (choise)
         {
         case 1:
-            cout << "Введите логин: " << endl;
+            cout << "-----------------------------" << endl;
+            cout << "-------- Регистрация --------" << endl;
+            cout << "Введите логин: ";
             cin >> login;
-            cout << "Введите пароль: " << endl;
+            cout << "Введите пароль: ";
             cin >> password;
-            cout << "Введите имя: " << endl;
+            cout << "Введите имя: ";
             cin >> name;
-            // функция регистрации пользователя
-            chat.registerUser(login, password, name);
-            // cout << "Вы успешно зарегистрировались" << endl;
+            chat.registerUser(login, password, name);           // метод регистрации пользователя
+            cout << "-----------------------------" << endl;
             cout << endl;
             break;
         case 2:
-            cout << "Введите логин: " << endl;
+            cout << "-----------------------------" << endl;
+            cout << "-------- Авторизация --------" << endl;
+            cout << "Введите логин: ";
             cin >> login;
-            cout << "Введите пароль: " << endl;
+            cout << "Введите пароль: ";
             cin >> password;
-            // функция авторизации пользователя
-            chat.loginUser(login, password);
+            chat.loginUser(login, password);                    // метод авторизации пользователя
+            cout << "-----------------------------" << endl;
             cout << endl;
             break;
         case 3:
-            cout << "Введите получателя:" << endl;
+        cout << "-----------------------------" << endl;
+            cout << "Введите получателя: ";
             cin >> login;
-            cout << "Введите текст сообщения: " << endl;
+            cout << "Введите имя отправителя: ";
+            cin >> name;
+            cout << "Введите текст сообщения: ";
             cin >> text;
-            // функция ввода сообщения и отправки пользоавтелю
-            chat.sendToUser(login, text);
+            chat.sendToUser(login, name, text);                 // метод ввода сообщения и отправки пользоавтелю
+            cout << "-----------------------------" << endl;
             cout << endl;
             break;
         case 4:
-            cout << "Отправить сообщение в чат" << endl;
-            // функция ввода сообщения и отправки в чат
-            
+            cout << "-----------------------------" << endl;
+            cout << "Все сообщения" << std::endl;
+            chat.viewChat();                                    // метод просмотра всех сообщений
+            cout << "-----------------------------" << endl;
+            cout << endl;
+            break;
+        case 5:
+            cout << "-----------------------------" << endl;
+            cout << "Все пользователи" << endl;
+            chat.displayUsers();                                // метод просмотра пользователей
+            cout << "-----------------------------" << endl;
             cout << endl;
             break;
         default:
