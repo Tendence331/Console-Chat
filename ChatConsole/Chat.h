265a1fcd020cfd59a59722ada4748b6546a05e0b
Vyacheslav <lance110741@gmail.com>
2025-02-01 01:22:58 +0300

    Корректировка

--- ChatConsole/Chat.h
+++ ChatConsole/Chat.h
@@ -11,14 +11,16 @@ class Chat
 public:
     Chat() = default;
 
-    void registerUser(std::string& login, std::string& password, std::string& name);   // регистрация пользователя
-    bool loginUser(std::string& login, std::string& password);   // авторизация пользователя должен возвращать true если удачно, если нет то false
-     
-    void sendToUser(std::string& recipient, std::string& text);          // отправка сообщения пользователю - сначала надо if(!currentUsers) Вы не авторизированы
-    void sendToChat();          // отправка сообщения в чат - сначала надо if(!currentUsers) Вы не авторизированы
+    void registerUser(std::string& login, std::string& password, std::string& name);          // регистрация пользователя
+    bool loginUser(std::string& login, std::string& password);                                // авторизация пользователя должен возвращать true если удачно, если нет то false
+    
+    void displayUsers();                                                                      // вывод пользователей
+
+    void sendToUser(std::string& recipient, std::string& sender, std::string& text);          // отправка сообщения пользователю
+    void viewChat();                                                                          // отправка сообщения в чат
     ~Chat() = default;
 private:
-    User *_currentUsers = {nullptr};    // текущий пользователь                         
+    User *_currentUsers = {nullptr};    // текущий пользователи                       
     std::shared_ptr<User> user;         // зарегистрированные пользователи
     std::shared_ptr<Message> message;   // сообщения
 };
