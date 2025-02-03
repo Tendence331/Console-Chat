08bb408b9d4ebadc5d4cf17e2729f9d793269c99
Vyacheslav <lance110741@gmail.com>
2025-02-04 00:45:24 +0300

    Add get/set, add sh_ptr next

--- ChatConsole/Message.h
+++ ChatConsole/Message.h
@@ -12,15 +12,18 @@ public:
     std::string getRecipient() const;
     std::string getText() const;
     std::string getSender() const;
+    std::shared_ptr<Message> getnextMessage() const;
 
     //setter
     void setRecipient(const std::string recipient);
     void setText(const std::string text);
     void setSender(const std::string sender);
+    void setNextMessage(std::shared_ptr<Message> next);
 
     void sendMessages();        // просмотр отправленного сообщения
 private:
     std::string _recipient;     // получатель
     std::string _text;          // сообщение
     std::string _sender;        // отправитель
+    std::shared_ptr<Message> _nextMess;
 };
