b246ee5a7e4ece7531f062229b04b342e33532cc
Vyacheslav <lance110741@gmail.com>
2025-02-04 00:43:24 +0300

    Update functions

--- ChatConsole/Chat.cpp
+++ ChatConsole/Chat.cpp
@@ -11,9 +11,10 @@ void Chat::registerUser(std::string &login, std::string &password, std::string &
             std::cout << "Пройдите регистрацию снова!" << std::endl;
             return;
         }
+        counter = counter->getNextUser();
     }
     std::shared_ptr<User> newUser = std::make_shared<User>(name, login, password);              // Добавляем нового пользователя
-    newUser->setLogin(login);                                                                   // Новый пользователь указывает на предыдущего
+    newUser->setNextUser(user);                                                                   // Новый пользователь указывает на предыдущего
     user = newUser;                                                                             // Новый пользователь становится началом списка
     std::cout << "Пользователь " << "[" << name << "]" << " зарегистрирован!" << std::endl;
 }
@@ -29,6 +30,7 @@ bool Chat::loginUser(std::string &login, std::string &password)                 
             std::cout << "Пользователь " << "[" << counter->getName() << "]" << " успешно вошел" << std::endl;   
             return true;
         }
+        counter = counter->getNextUser();
     }
     std::cout << "Ошибка ввода логина и пароля" << std::endl;
     return false;
@@ -36,31 +38,46 @@ bool Chat::loginUser(std::string &login, std::string &password)                 
 
 void Chat::displayUsers()
 {
-    std::cout << "Пользователь: " << _currentUsers->getName() << " логин: " << _currentUsers->getLogin() << std::endl;
+    std::shared_ptr<User> _currentUsers = user;
+    if (!_currentUsers)
+    {
+        return;
+    }
+    while (_currentUsers)
+    {
+        std::cout << "Пользователь: [ " << _currentUsers->getName() << " ] Логин: [ " << _currentUsers->getLogin() << " ]" << std::endl;
+        _currentUsers = _currentUsers->getNextUser();
+    }
 }
 
 void Chat::sendToUser(std::string &recipient, std::string &sender, std::string &text)
 {
-    while (!_currentUsers)
+    std::shared_ptr<Message> msg = std::make_shared<Message>(recipient, sender, text);
+    if(!_currentUsers)
     {
         std::cout << "Вы не авторизированы!" << std::endl;
-        break;
+        return;
     }
-    std::shared_ptr<Message> msg = std::make_shared<Message>(recipient, sender, text);
+    msg->setNextMessage(message);
+    message = msg;
     msg->sendMessages();
     
 }
 
 void Chat::viewChat()
 {
-    std::shared_ptr<Message> msg = message;
-    while (_currentUsers)
+    if (!_currentUsers)
     {
-        if (msg->getRecipient() == "all")
-        {
-            std::cout << "Чат: " << msg->getSender() << ": " << msg->getText() << std::endl;
-        }
+        std::cout << "Вы не авторизированы!" << std::endl;
+        return;
     }
+    std::shared_ptr<Message> msg = message;
     
+    while (msg)
+    {
+        std::cout << "Чат: " << msg->getSender() << ": " << msg->getText() << std::endl;
+        msg = msg->getnextMessage();
+        msg->sendMessages();
+    }
 }
 
